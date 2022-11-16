bdea511cfe64f060525e7777214e5ada927544f3
JeanOdon <118370757+JeanOdon@users.noreply.github.com>
2022-11-16 15:56:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+})
+
+sr.reveal('.about,.skills,.services,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about',{delay:200, origin:'bottom'})
